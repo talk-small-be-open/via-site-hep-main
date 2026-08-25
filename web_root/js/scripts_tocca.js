22c4db31d5bd034293d79d45b571b257f59ec22b
Andreas Brodbeck <andreas.brodbeck@mindclue.ch>
2026-08-25 14:50:18 +0200

    minor

--- web_root/js/scripts_tocca.js
+++ web_root/js/scripts_tocca.js
@@ -170,6 +170,7 @@ function processHtmlDocument(contextElement) {
 
 	if ( ! (typeof JoelPurra === 'undefined') ) {
 		$('input.clozeTextPlaceholder', contextElement).plusAsTab();
+		$('div.textListInput input.textInput', contextElement).plusAsTab();
 	}
 }
 
@@ -184,6 +185,209 @@ function scrollToExerciseTask(taskTitleElement) {
 	window.scroll({behavior: 'smooth', top: (elementPosition - targetPosition) });
 
 }
+
+// function addTextToCursor(text) {
+
+//   const element = document.activeElement;
+//   const caretPos = element.selectionStart;
+//   const textAreaTxt = element.value;
+	
+//   element.value = textAreaTxt.substring(0, caretPos) + text + textAreaTxt.substring(caretPos);
+// 	element.selectionStart = caretPos + txtToAdd.length
+// }
+
+
+//
+// Helpers for VIATextListInputTask
+//
+
+function listInput_addText(id, text, isWholeWord) {
+
+	var textToAdd = text;
+	const main = document.getElementById(id);
+	const element = $(main).data('lastInputElement') || $(main).find("div.textListLines input.textInput")[0];
+	const position = $(main).data('lastPosition') || 0;
+  const originalText = element.value;
+
+	// Auto add space?
+	if ( isWholeWord && (position > 0) && (originalText.at(position-1) != " ") ) {
+		textToAdd = ' ' + textToAdd;
+	}
+	
+  element.value = originalText.substring(0, position) + textToAdd + originalText.substring(position);
+	element.selectionStart = position + textToAdd.length;
+	element.selectionEnd = element.selectionStart;
+	$(main).data('lastPosition', element.selectionStart);
+
+	element.focus();
+	
+}
+
+function listInput_addSpace(id) {
+	listInput_addText(id, " ");
+}
+
+
+function listInput_gotoNextLine(id) {
+
+	const main = document.getElementById(id);
+	const emptyInputs = $(main).find("div.textListLines input.textInput").filter(function() {
+    return !this.value;
+	});
+
+	const element = emptyInputs[0];
+
+	if (element) {
+		$(main).data('lastInputElement', element);
+		$(main).data('lastPosition', 0);
+		element.focus();
+	}
+	
+}
+
+//
+// Helpers for glossary
+//
+
+function glossary_scrollTo(glossary, entry) {
+	const glossaryBody = $("div.glossaryBody", glossary).get(0);
+	$(glossaryBody).addClass('scrolling');
+	glossaryBody.scroll({behavior: 'smooth', top: $(entry).position().top });	
+}
+
+function glossary_jumpTo(glossaryId, jumpId) {
+	const glossary = document.getElementById(glossaryId);
+	const entry = document.getElementById(jumpId);
+
+	glossary_reset(glossaryId);
+	$(entry).addClass('internalGoto');
+
+	glossary_scrollTo(glossary, entry);
+	
+}
+
+function glossary_jumpToReference(clickedElement, jumpId) {
+	const entry = document.getElementById(jumpId);
+
+	if (entry) {
+		const glossary = $(clickedElement).closest('div.glossary').get(0);
+//		const glossaryBody = $("div.glossaryBody", glossary).get(0);
+	
+		glossary_reset(glossary.id);
+		$(entry).addClass('internalGoto');
+
+		glossary_scrollTo(glossary, entry);
+	}
+}
+
+
+// function glossary_internalGotoReference(clickedElement, searchReferenceCode) {
+// 	const glossary = $(clickedElement).closest('div.glossary').get(0);
+// 	const searchText = searchReferenceCode.toLowerCase();
+
+// 	// Search entry by title exact match
+// 	// OPTIMIZE: Stop after first hit. jquery cant do that?
+// 	const all = $(".glossaryEntry", glossary).filter(function() {
+
+// 		const entry = $(this);
+// 		const referenceCode = $(".referenceCode", entry);
+		
+// 		// Check content
+//     return referenceCode.text().toLowerCase() == searchText;
+// 	});
+
+// 	const entry = all.first();
+
+// 	if (entry) {
+// 		const glossaryBody = $("div.glossaryBody", glossary).get(0);
+		
+// 		glossary_reset(glossary.id);
+// 		entry.addClass('internalGoto');
+// 		glossaryBody.scroll({behavior: 'smooth', top: entry.position().top });
+// 	}
+	
+// }
+
+
+// Put the glossary in the initial state, no filter, no highlight, no scrolling
+function glossary_reset(glossaryId) {
+
+	const glossary = document.getElementById(glossaryId);
+	const glossaryBody = $("div.glossaryBody", glossary);
+	const searchInput = $("input.searchText", glossary);
+
+	$(searchInput).val('');
+	$(".glossaryEntry", glossary).removeClass('searchFound searchFoundInTitle searchIsTitle searchNotFound internalGoto');
+	glossaryBody.removeClass('scrolling');
+
+	
+}
+
+function glossary_liveSearch(glossaryId) {
+
+	const glossary = document.getElementById(glossaryId);
+	const searchInput = $("input.searchText", glossary);
+	const text = searchInput.val().toLowerCase();
+	var fullHitElem;
+
+	// Remove search completely, if empty search text
+	if ( !text ) {
+		glossary_reset(glossaryId);
+		return;
+	}
+
+	// Reset scrolling UI, normally not scrolling.
+	const glossaryBody = $("div.glossaryBody", glossary);
+	glossaryBody.removeClass('scrolling');
+
+	// Search text in all rows
+	$(".glossaryEntry", glossary).each(function() {
+
+		const elem = $(this);
+		const title = $(".title", elem).text().toLowerCase();
+		const simpleTerm = elem.get(0).dataset.simpleTerm.toLowerCase();
+		var found = false;
+
+		// Reset attributes from maybe previous search
+		elem.removeClass('searchFoundInTitle searchIsTitle internalGoto');
+	
+		// Check full text content
+    if ( elem.text().toLowerCase().indexOf(text) >= 0 ) {
+			
+			found = true;
+
+			// Sub-check, if hit is in the title
+			if ( title.indexOf(text) >= 0 ) {
+				elem.addClass('searchFoundInTitle')
+			}
+
+		}
+
+		// Check, if hit is = title, either by shown title with accents or by simple term with normalized accents
+		// Jump to by scroll
+		if ( (title == text) || (simpleTerm == text) ) {
+			found = true;
+			elem.addClass('searchIsTitle');
+			fullHitElem = elem.get(0);
+		}
+
+		// Switch visibility down here, for smooth hide/show
+		if (found) {
+			elem.addClass('searchFound').removeClass('searchNotFound');
+		} else {
+			elem.addClass('searchNotFound').removeClass('searchFound');
+		}
+
+
+	});
+
+	if (fullHitElem) {
+		glossary_scrollTo(glossary, fullHitElem);
+	}
+
+}
+
+
 
 
 /* Haupt JS init */
